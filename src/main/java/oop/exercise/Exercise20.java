07cf595198975a77efa996058a55852947755a5c
joshfeds14 <joshfeds14@gmail,com>
2021-06-05 15:31:34 -0400

    Exercise 20

---
+++ src/main/java/oop/exercise/Exercise20.java
@@ -0,0 +1,12 @@
+package oop.exercise;
+import java.text.NumberFormat;
+import java.util.Scanner;
+import java.util.Locale;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Joshua Federman
+ */
+public class Exercise20 {
+    static Scanner input = new Scanner(System.in);
+
+}
