4b53cb7ee3b63ce18dfb2df8aef157295831a2ab
joshfeds14 <joshfeds14@gmail,com>
2021-06-06 18:09:55 -0400

    Exercise 20

--- src/main/java/oop/exercise/Exercise20.java
+++ src/main/java/oop/exercise/Exercise20.java
@@ -5,30 +5,6 @@ import java.util.Locale;
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
  *  Copyright 2021 Joshua Federman
- * More complex programs may have decisions nested in other decisions,
- * so that when one decision is made, additional decisions must be made.
-
-Create a tax calculator that handles multiple states and multiple counties within each state.
-* The program prompts the user for the order amount and the state where the order will be shipped.
-
-Wisconsin residents must be changed 5% sales tax with an additional county-level charge.
-* For Wisconsin residents, prompt for the county of residence.
-For Eau Claire county residents, add an additional 0.005 tax.
-For Dunn county residents, add an additional 0.004 tax.
-Illinois residents must be charged 8% sales tax with no additional county-level charge.
-All other states are not charged tax.
-The program then displays the tax and the total for Wisconsin and Illinois residents but just the total
-* for everyone else.
-
-Example Output
-What is the order amount? 10
-What state do you live in? Wisconsin
-What county do you live in? Dane
-The tax is $0.50.
-The total is $10.50.
-Constraints
-Ensure that all money is rounded up to the nearest cent.
-Use a single output statement at the end of the program to display the program results.
  */
 public class Exercise20 {
     static Scanner input = new Scanner(System.in);
@@ -51,7 +27,7 @@ public class Exercise20 {
     }
     public static void results(String a, String state, String county){
         float amount = Float.parseFloat(a);
-        float total = 0;
+        float total;
         float tax = 0;
         boolean isTaxState = true;
         if(state.equalsIgnoreCase("Wisconsin")) {
@@ -60,7 +36,7 @@ public class Exercise20 {
                 total = amount + (amount * tax);
 
             }
-            if(county.equalsIgnoreCase("Dunn")) {
+            else if(county.equalsIgnoreCase("Dunn")) {
                 tax = (WISCONSIN_TAX + DUNN_TAX);
                 total = amount + (amount * tax);
             }
@@ -80,6 +56,7 @@ public class Exercise20 {
 
         Locale convert = new Locale("en", "US");
         NumberFormat printDollar = NumberFormat.getCurrencyInstance(convert);
+
         String result = isTaxState ? "The tax is " + printDollar.format(tax * amount) +
                         ".\nThe total is " + printDollar.format(total) : "The total is " +
                             printDollar.format(total) + ".";
